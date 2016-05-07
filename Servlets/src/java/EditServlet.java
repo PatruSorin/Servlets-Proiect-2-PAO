ab5b806506577b6f73fe2c7361b2e18d82063dd3
Patru Sorin <patrusorin95@gmail.com>
2016-05-07 16:04:27 +0300

    edit

--- Servlets/src/java/EditServlet.java
+++ Servlets/src/java/EditServlet.java
@@ -21,8 +21,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Patru Sorin
  */
-@WebServlet(urlPatterns = {"/SearchServlet"})
-public class SearchServlet extends HttpServlet {
+@WebServlet(urlPatterns = {"/EditServlet"})
+public class EditServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -45,50 +45,19 @@ public class SearchServlet extends HttpServlet {
     String oras ;
     String judet ;
     String cod_postal;
-        
-     //Introducerea datelor din formular in variabile
-        
-        nume = request.getParameter("nume");
-        if(nume.isEmpty())
-            nume="null";
-        
-        prenume = request.getParameter("prenume");
-        if(prenume.isEmpty())
-            prenume="null";
-        
-        telefon_mobil = request.getParameter("telefon_mobil");
-        if(telefon_mobil.isEmpty())
-            telefon_mobil="null";
-        
-        telefon_fix = request.getParameter("telefon_fix");
-        if(telefon_fix.isEmpty())
-            telefon_fix="null";
-        
-        email = request.getParameter("email");
-        if(email.isEmpty())
-            email="null";
-        
-        adresa = request.getParameter("adresa");
-        if(adresa.isEmpty())
-            adresa="null";
-        
-        oras = request.getParameter("oras");
-        if(oras.isEmpty())
-            oras="null";
-        
-        judet = request.getParameter("judet");
-        if(judet.isEmpty())
-            judet="null";
-        
-        cod_postal = request.getParameter("cod_postal");
-        if(cod_postal.isEmpty())
-            cod_postal="null";
-        
+    
+    String nume_search;
+    String prenume_search;
+           
+        try{
+        //Introducerea dateleor din formularul de cautare a persoanei ce trebuie editata
+        nume_search=request.getParameter("nume_search");
+        prenume_search=request.getParameter("prenume_search");
+            
+            
         String url  = "jdbc:mysql://localhost:3306/agenda";
         String user = "persoana";
         String password = "persoana";
-        
-        try{
             
         //Incarcam driver
         Class.forName("com.mysql.jdbc.Driver");
@@ -104,68 +73,91 @@ public class SearchServlet extends HttpServlet {
         //Cauta in baza de date valorile din variabile
         
         String sql = "select * from contacte where "
-                    +"nume='"+nume+"' or prenume='"+prenume+"' or telefon_mobil= '"+telefon_mobil+"'"
-                    +"or telefon_fix='"+telefon_fix+"' or email='"+email+"' or oras='"+oras+"'"
-                    +"or judet='"+judet+"' or cod_postal='"+cod_postal+"'";
-        
+                    +"nume='"+nume_search+"' and prenume='"+prenume_search+"'";
+                    
         ResultSet myRs = myStmt.executeQuery(sql);
         
         
+        //Introducerea datelor din formularul de editare in variabile
+        
+        nume = request.getParameter("nume");
+        if(nume.isEmpty())
+            nume=myRs.getString("nume");
         
         
-    //Afiseaza rezultatele cautarii    
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SearchServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Rezultate cautare:</h1>");
-            
-            //Tabel cu rezultatele obtinute
-            out.println("<table border=\"1\" style=\"width:100%\">");
-            
-            out.println("<tr>");
-            out.println("<th>Nume</th>");
-            out.println("<th>Prenume</th>");
-            out.println("<th>Telefon mobil</th>");
-            out.println("<th>Telefon fix</th>");
-            out.println("<th>Email</th>");
-            out.println("<th>Adresa</th>");
-            out.println("<th>Oras</th>");
-            out.println("<th>Judet</th>");
-            out.println("<th>Cod postal</th>");
-            out.println("</tr>");
-            
-            while(myRs.next()){
-            out.println("<tr>");
-            out.println("<td>"+myRs.getString("nume")+"</td>");
-            out.println("<td>"+myRs.getString("prenume")+"</td>");
-            out.println("<td>"+myRs.getString("telefon_mobil")+"</td>");
-            out.println("<td>"+myRs.getString("telefon_fix")+"</td>");
-            out.println("<td>"+myRs.getString("email")+"</td>");
-            out.println("<td>"+myRs.getString("adresa")+"</td>");
-            out.println("<td>"+myRs.getString("oras")+"</td>");
-            out.println("<td>"+myRs.getString("judet")+"</td>");
-            out.println("<td>"+myRs.getString("cod_postal")+"</td>");
-            out.println("</tr>");
-            }
-          
- 
-            out.println("</table>");
+        prenume = request.getParameter("prenume");
+        if(prenume.isEmpty())
+            prenume=myRs.getString("prenume");
         
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        telefon_mobil = request.getParameter("telefon_mobil");
+        if(telefon_mobil.isEmpty())
+            telefon_mobil=myRs.getString("telefon_mobil");
+        
+        
+        telefon_fix = request.getParameter("telefon_fix");
+        if(telefon_fix.isEmpty())
+            telefon_fix=myRs.getString("telefon_fix");
+        else if(telefon_fix.equals("-"))
+              telefon_fix="";
+        
+        email = request.getParameter("email");
+        if(email.isEmpty())
+            email=myRs.getString("email");
+        
+        
+        adresa = request.getParameter("adresa");
+        if(adresa.isEmpty())
+            adresa=myRs.getString("adresa");
+        else if(adresa.equals("-"))
+              adresa="";
+        
+        oras = request.getParameter("oras");
+        if(oras.isEmpty())
+            oras=myRs.getString("oras");
+        else if(oras.equals("-"))
+              oras="";
+        
+        judet = request.getParameter("judet");
+        if(judet.isEmpty())
+            judet=myRs.getString("judet");
+        else if(judet.equals("-"))
+              judet="";
+        
+        cod_postal = request.getParameter("cod_postal");
+        if(cod_postal.isEmpty())
+            cod_postal=myRs.getString("cod_postal");
+        else if(cod_postal.equals("-"))
+              cod_postal="";
+        
+        //Modificam datele in tabel
+        
+        sql="update contacte set "
+            +"nume='"+nume+"' , prenume='"+prenume+"' , telefon_mobil= '"+telefon_mobil+"' "
+            +", telefon_fix='"+telefon_fix+"' , email='"+email+"' , oras='"+oras+"' "
+            +", judet='"+judet+"' , cod_postal='"+cod_postal+"' , adresa='"+adresa+"' "
+            +"where nume='"+nume_search+"' and prenume='"+prenume_search+"'";
+        
+        myStmt.executeUpdate(sql);
         
         }
         catch(ClassNotFoundException | SQLException exc){
         
         }
         
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Modificari realizate cu succes!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
